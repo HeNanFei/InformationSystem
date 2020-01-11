91a66b3574c5b90c0a7109113cf93524d9b7a3d0
zjt <2531700373@qq.com>
2020-01-11 10:19:40 +0800

    完成用户分页

--- src/main/java/com/zjt/manager/controller/UserController.java
+++ src/main/java/com/zjt/manager/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.zjt.manager.controller;
-
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.zjt.manager.mapper.UserMapper;
 import com.zjt.manager.pojo.User;
 import com.zjt.manager.service.UserService;
 import com.zjt.manager.util.Bcry;
@@ -9,35 +11,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@Controller
+@RestController
 public class UserController {
-
+    @Autowired
+    private UserMapper userMapper;
    @Autowired
    private UserService userService;
-   @ResponseBody
+
    @RequestMapping("/usr/add")
    public String add(String user1){
        System.out.println(user1);
-
-
        User user2 = JSONObject.parseObject(user1, User.class);
-
-
        String password = Bcry.bcry(user2.getPassword());
        user2.setPassword(password);
        userService.insertUser(user2);
 
        return "添加成功";
    }
-
+    int temp = 0;
    @RequestMapping("/usr/list")
-    public List<User> list(){
+    public Map list(Integer page,Integer limit){
+       List<User> list1 = userMapper.selectByLimit((page-1)*limit, limit-1);
+       List<User> list = userService.list();
 
+       /*PageHelper.startPage(page,limit);
+       List<User> list = userService.list();
+       PageInfo<User> pageInfo = new PageInfo<>(list);
 
-       return userService.list();
+       List<User> list1 = pageInfo.getList();*/
+
+       Map<String,Object> map = new HashMap<>();
+       map.put("code",0);
+       map.put("msg","");
+       map.put("count",list.size());
+       map.put("data",list1);
+
+       return map;
    }
 
 
--- src/test/java/com/zjt/manager/ManagerApplicationTests.java
+++ src/test/java/com/zjt/manager/ManagerApplicationTests.java
@@ -1,7 +1,8 @@
 package com.zjt.manager;
 
+
+
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
